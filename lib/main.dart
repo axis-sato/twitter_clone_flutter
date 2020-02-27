9ab937e55015cb796d3b955cc07b7da34cd28531
sato <sato_m@mediaxis.jp>
2020-02-27 22:34:02 +0900

    DIの設定を別ファイルに切り出した

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:twitter_clone_flutter/core/infra/api.dart';
-import 'package:twitter_clone_flutter/core/services/tweet_service.dart';
-import 'package:twitter_clone_flutter/core/services/user_service.dart';
+import 'package:twitter_clone_flutter/provider_setup.dart';
 import 'package:twitter_clone_flutter/ui/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
@@ -11,17 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        Provider<Api>(create: (_) => Api()),
-        ProxyProvider<Api, TweetService>(
-          create: (context) => TweetService(),
-          update: (context, api, service) => service..api = api,
-        ),
-        ProxyProvider<Api, UserService>(
-          create: (context) => UserService(),
-          update: (context, api, service) => service..api = api,
-        ),
-      ],
+      providers: providers,
       child: MaterialApp(
         title: 'ツイッタークローン',
         theme: ThemeData(
---
+++ lib/provider_setup.dart
@@ -0,0 +1,25 @@
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
+import 'package:twitter_clone_flutter/core/infra/api.dart';
+import 'package:twitter_clone_flutter/core/services/tweet_service.dart';
+import 'package:twitter_clone_flutter/core/services/user_service.dart';
+
+List<SingleChildWidget> providers = [
+  ..._infra,
+  ..._services,
+];
+
+List<SingleChildWidget> _infra = [
+  Provider<Api>(create: (_) => Api()),
+];
+
+List<SingleChildWidget> _services = [
+  ProxyProvider<Api, TweetService>(
+    create: (context) => TweetService(),
+    update: (context, api, service) => service..api = api,
+  ),
+  ProxyProvider<Api, UserService>(
+    create: (context) => UserService(),
+    update: (context, api, service) => service..api = api,
+  ),
+];
